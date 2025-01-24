b49896008f72dd38d3e46c2cb95ceb2a10d3d6ef
anatalphonset <anatalphonset@gmail.com>
2025-01-24 16:06:46 +0530

    third

--- 23_1_2025.js
+++ 23_1_2025.js
@@ -1,8 +1,11 @@
+// to toUpperCase
 str = "anat"
 console.log(str.toUpperCase(str))
 
+// check "javascript" in string
 str = "anat javascript"
 console.log(str.includes("javascript"))
 
+// remove vowels
 str = "anat javascript"
 console.log(str.replace(/[aeiouAEIOU]/g,''))
